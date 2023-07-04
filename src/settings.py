9ae95e0a85aa621af699ffa52cd0c67ed9cd8f1e
almaz <almazgimaev@gmail.com>
2023-07-04 21:56:42 +0300

    change preview image id

--- src/settings.py
+++ src/settings.py
@@ -20,7 +20,7 @@ RELEASE_YEAR: int = 2019
 HOMEPAGE_URL: str = "https://www.kaggle.com/datasets/niccha/malaria-segmentation"
 # e.g. "https://some.com/dataset/homepage"
 
-PREVIEW_IMAGE_ID: int = 1461718
+PREVIEW_IMAGE_ID: int = 1473044
 # This should be filled AFTER uploading images to instance, just ID of any image.
 
 GITHUB_URL: str = "https://github.com/dataset-ninja/malaria-segmentation"
